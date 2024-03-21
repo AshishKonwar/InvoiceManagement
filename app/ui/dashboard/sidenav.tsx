26fb1e74c75d025cbc4b3826ad9ef3eb19836ba5
AshishKonwar <57911472+AshishKonwar@users.noreply.github.com>
2024-03-21 15:39:16 +0530

    first_commit

---
+++ app/ui/dashboard/sidenav.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import NavLinks from '@/app/ui/dashboard/nav-links';
+import AcmeLogo from '@/app/ui/acme-logo';
+import { PowerIcon } from '@heroicons/react/24/outline';
+import icon from '@/public/ashish-high-resolution-logo-transparent.png';
+import Image from 'next/image';
+
+export default function SideNav() {
+  return (
+    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+      <Link
+        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-32"
+        href="/"
+      >
+        <div className="hidden w-32 text-white md:block">
+          <Image src={icon} width={91} height={91} alt="Icon" />
+        </div>
+        <div className="block w-32 text-white md:hidden">
+          <Image src={icon} width={51} height={51} alt="Icon" />
+        </div>
+      </Link>
+      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+        <NavLinks />
+        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+        <form>
+          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+            <PowerIcon className="w-6" />
+            <div className="hidden md:block">Sign Out</div>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
